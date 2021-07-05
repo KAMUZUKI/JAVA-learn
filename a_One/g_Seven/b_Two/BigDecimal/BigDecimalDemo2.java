4783478c3124460ff54322e7a4f5f0a2670be231
KAMUZUKI <1437487442@qq.com>
2021-07-05 19:51:37 +0800

    BigDecimal

---
+++ a_One/g_Seven/b_Two/BigDecimal/BigDecimalDemo2.java
@@ -0,0 +1,18 @@
+package a_One.g_Seven.b_Two.BigDecimal;
+
+// import java.math.BigDecimal;
+
+public class BigDecimalDemo2 {
+    public static void main(String[] args) {
+        // BigDecimal bd1 = new BigDecimal("10.0");
+        // BigDecimal bd2 = new BigDecimal("3.0");
+        // BigDecimal divide = bd1.divide(bd2, 2, BigDecimal.ROUND_UP);
+        // System.out.println(divide);
+
+        // BigDecimal divide2 = bd1.divide(bd2, 2, BigDecimal.ROUND_FLOOR);
+        // System.out.println(divide2);
+
+        // BigDecimal divide3 = bd1.divide(bd2, 2, BigDecimal.ROUND_HALF_UP);
+        // System.out.println(divide3);
+    }
+}
