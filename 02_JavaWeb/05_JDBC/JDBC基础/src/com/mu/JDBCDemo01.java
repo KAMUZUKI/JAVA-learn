c89caef1958655474e639699d125e67937cd6550
KAMUZUKI <1437487442@qq.com>
2022-03-06 15:52:30 +0800

    JDBC

---
+++ 02_JavaWeb/05_JDBC/JDBC基础/src/com/mu/JDBCDemo01.java
@@ -0,0 +1,32 @@
+package com.mu;
+
+import java.sql.*;
+
+public class JDBCDemo01 {
+    public static void main(String[] args) throws Exception{
+        //1.导入jar包
+
+        //2.注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //3.获取连接
+        Connection con = DriverManager.getConnection("jdbc:mysql://192.168.23.129:3306/db2","root","muzuki");
+
+        //4.获取执行者对象
+        Statement stat = con.createStatement();
+
+        //5.执行sql语句，并且接收结果
+        String sql = "SELECT * FROM student";
+        ResultSet rs = stat.executeQuery(sql);
+
+        //6.处理结果
+        while (rs.next()){
+            System.out.println(rs.getInt("age") + "\t" + rs.getString("name"));
+        }
+
+        //7.释放资源
+        con.close();
+        stat.close();
+        rs.close();
+    }
+}
