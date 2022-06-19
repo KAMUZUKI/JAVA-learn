5c8d5dd9545fb079ee96fa9960aeb7c8b0de9753
KAMUZUKI <1437487442@qq.com>
2022-06-19 18:33:00 +0800

    springboot-06-security

--- PopularFramework/Spring/_6_SpringBoot/springboot-06-security/src/main/java/com/mu/Config/SecurityConfig.java
+++ PopularFramework/Spring/_6_SpringBoot/springboot-06-security/src/main/java/com/mu/Config/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.mu.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,10 +15,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/level1/**").hasRole("vip1")
                 .antMatchers("/level2/**").hasRole("vip2")
                 .antMatchers("/level3/**").hasRole("vip3");
-        // 开启登录
+
         http.formLogin();
-        // 开启注销
-        http.logout();
     }
 
     @Override
--- PopularFramework/Spring/_6_SpringBoot/springboot-06-security/src/main/java/com/mu/controller/RouterController.java
+++ PopularFramework/Spring/_6_SpringBoot/springboot-06-security/src/main/java/com/mu/controller/RouterController.java
@@ -3,10 +3,7 @@ package com.mu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@ResponseBody
 @Controller
 public class RouterController {
     @RequestMapping({"/","/index"})
